ecce730ed1549835a91047f3dba0d41f0b086fe2
Gabrielle Almeida Cuba <gabrielle.cuba@estudante.ufla.br>
2020-06-30 15:55:09 -0300

    App ToDo finalizado

--- backend/src/config/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req,res,next){
-    res._header('Acess-Control-Allow-Origin','*')
-    res._header('Acess-Control-Allow-Origin','GET,POST,OPTIONS,PUT,PATCH,DELETE')
-    res._header('Acess-Control-Allow-Origin','Origin, X-Request-With, Content-Type, Accept')
-    next()
-}
